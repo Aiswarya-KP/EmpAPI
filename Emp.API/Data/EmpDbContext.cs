2f8e03551e8b91b9b8594686efabccb2cf48caf0
Aiswarya Kizhakkedath <aiswaryakp2k@gmail.com>
2023-05-08 10:41:17 +0530

    updatedAPI

---
+++ Emp.API/Data/EmpDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Emp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Emp.API.Data
+{
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
